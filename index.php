25c20a819d000bb6e1bd94e540213ab96e8a5f8f
Sandun Sahiru <134186922+sandunsahiru@users.noreply.github.com>
2024-09-13 22:12:42 +0530

    Index File

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+// index.php
+
+// Autoload controllers and models
+spl_autoload_register(function ($class_name) {
+    if (file_exists('controllers/' . $class_name . '.php')) {
+        require_once 'controllers/' . $class_name . '.php';
+    } elseif (file_exists('models/' . $class_name . '.php')) {
+        require_once 'models/' . $class_name . '.php';
+    }
+});
+
+// Start session
+session_start();
+
+// Basic routing
+$page = isset($_GET['page']) ? $_GET['page'] : 'dashboard';
+
+switch ($page) {
+    case 'dashboard':
+        $controller = new DashboardController();
+        $controller->index();
+        break;
+    // Add more cases for other pages like appointments, doctors, etc.
+    default:
+        // Handle 404
+        echo 'Page not found';
+        break;
+}
+?>
