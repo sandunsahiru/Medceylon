065b1a29d7bb6de4e5be7ccf487a2bc79e06b106
Sandun Sahiru <134186922+sandunsahiru@users.noreply.github.com>
2024-09-13 22:16:06 +0530

    General Doctor Dashboard

---
+++ views/templates/footer.php
@@ -0,0 +1,4 @@
+<!-- views/templates/footer.php -->
+<script src="assets/js/script.js"></script>
+</body>
+</html>
---
+++ views/templates/topbar.php
@@ -0,0 +1,7 @@
+<!-- views/templates/topbar.php -->
+<div class="topbar">
+    <div class="topbar-right">
+        <span class="notification-icon">🔔</span>
+        <img src="assets/images/user_avatar.png" alt="User Avatar" class="user-avatar">
+    </div>
+</div>
