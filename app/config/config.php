330990c3f15f119ef93ed76bff0c6415906d89ba
Sandun Sahiru <134186922+sandunsahiru@users.noreply.github.com>
2024-11-22 22:38:04 +0530

    MVC Update

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'http://localhost/medceylon');
+    // Site Name 
+    define('SITENAME', 'MedCeylon');
+
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'medceylon');
+
+?>
